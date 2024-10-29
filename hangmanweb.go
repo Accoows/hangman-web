9f1ea857130b2052c40fef23c2c88de211fcb8b8
Accoow <gnzlsarthur@gmail.com>
2024-10-29 12:40:00 +0100

    Add Server/Web

--- hangmanweb.go
+++ hangmanweb.go
@@ -14,7 +14,7 @@ var revealed []bool
 var attemptsLeft int
 var lettersRevealed map[rune]bool
 
-func initializeGame() {
+func initializeHangman() {
 	// Initialisation du jeu
 	words, err := Hangmanclassic.ReadFileLines("words.txt", false)
 	if err != nil {
@@ -27,25 +27,7 @@ func initializeGame() {
 }
 
 func renderTemplate(w http.ResponseWriter, data interface{}) {
-	tmpl := `
-	<!DOCTYPE html>
-	<html>
-	<head>
-		<title>Hangman Game</title>
-	</head>
-	<body>
-		<h1>Jeu du Pendu</h1>
-		<p>Mot à deviner : {{.WordDisplay}}</p>
-		<p>Tentatives restantes : {{.AttemptsLeft}}</p>
-		<form action="/hangman" method="POST">
-			<label for="letter">Entrez une lettre :</label>
-			<input type="text" name="letter" maxlength="1" required>
-			<button type="submit">Vérifier</button>
-		</form>
-	</body>
-	</html>
-	`
-	t, err := template.New("hangman").Parse(tmpl)
+	t, err := template.ParseFiles("Templates/hangman.tmpl")
 	if err != nil {
 		http.Error(w, "Erreur interne du serveur", http.StatusInternalServerError)
 		return
@@ -98,14 +80,14 @@ func Hangman(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if attemptsLeft <= 0 || Hangmanclassic.AllRevealed(revealed) {
-		initializeGame() // Réinitialiser le jeu si toutes les lettres sont trouvées ou si les tentatives sont épuisées
+		initializeHangman() // Réinitialiser le jeu si toutes les lettres sont trouvées ou si les tentatives sont épuisées
 	}
 
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func main() {
-	initializeGame()
+	initializeHangman()
 
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/hangman", Hangman)
