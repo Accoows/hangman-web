d6bc9e4b5e4785d4c864544d754464df4f844261
Accoow <gnzlsarthur@gmail.com>
2024-10-29 12:09:16 +0100

    Add Web

--- Hangmanclassic/hangman.go
+++ Hangmanclassic/hangman.go
@@ -2,20 +2,17 @@ package Hangmanclassic
 
 import (
 	"bufio"
-	"fmt"
 	"math/rand"
 	"os"
 	"strings"
 	"unicode"
-
-	"github.com/fatih/color"
 )
 
-var maxtentative int = 10
-var hangmanStages []string
+var Maxtentative int = 10
+var HangmanStages []string
 
 // Fonction pour lire les lignes d'un fichier et du hangman.
-func readFileLines(filename string, isHangmanStages bool) ([]string, error) {
+func ReadFileLines(filename string, isHangmanStages bool) ([]string, error) {
 	file, err := os.Open(filename)
 	if err != nil {
 		return nil, err
@@ -49,7 +46,7 @@ func readFileLines(filename string, isHangmanStages bool) ([]string, error) {
 }
 
 // Choisir un mot et révéler quelques lettres
-func findWord(words []string) (string, []bool) {
+func FindWord(words []string) (string, []bool) {
 	word := words[rand.Intn(len(words))]
 	revealed := make([]bool, len(word))
 
@@ -73,110 +70,30 @@ func findWord(words []string) (string, []bool) {
 	return word, revealed
 }
 
-// Fonction pour afficher le mot
-func displayWord(word string, revealed []bool, lastCorrectLetter rune) {
+// Fonction pour afficher le mot en tant que chaîne
+func DisplayWord(word string, revealed []bool, lastCorrectLetter rune) string {
+	var display strings.Builder
 	for i, letter := range word {
 		if revealed[i] {
-			if letter == lastCorrectLetter { // Vérifie si la lettre est la dernière correcte
-				color.Set(color.FgGreen) // Change la couleur de la lettre
-				fmt.Printf("%c ", letter)
-				color.Unset()
+			if letter == lastCorrectLetter {
+				display.WriteRune(letter) // Ajoute la lettre révélée
 			} else {
-				fmt.Printf("%c ", letter)
+				display.WriteRune(letter)
 			}
 		} else {
-			fmt.Print("_ ")
+			display.WriteRune('_') // Affiche "_" pour les lettres masquées
 		}
+		display.WriteRune(' ')
 	}
-	fmt.Println()
+	return display.String()
 }
 
 // Fonction pour vérifier si toutes les lettres sont révélées
-func allRevealed(revealed []bool) bool {
+func AllRevealed(revealed []bool) bool {
 	for _, r := range revealed {
 		if !r {
 			return false
 		}
 	}
 	return true
-}
-
-func main() {
-	var err error
-	if hangmanStages, err = readFileLines("hangman.txt", true); err != nil {
-		fmt.Println("Erreur lors de la lecture des étapes :", err)
-		return
-	}
-
-	words, err := readFileLines("words.txt", false)
-	if err != nil {
-		fmt.Println("Erreur lors de la lecture du fichier :", err)
-		return
-	}
-
-	word, revealed := findWord(words) //Choix du mot aléatoire
-	tentatives := maxtentative
-	lettersRevealed := make(map[rune]bool) //Trace des lettres déjà révélées
-	var lastCorrectLetter rune
-
-	fmt.Println("Bonne chance, vous avez", maxtentative, "tentatives.")
-	displayWord(word, revealed, lastCorrectLetter)
-
-	user := bufio.NewReader(os.Stdin)
-	for tentatives > 0 {
-		fmt.Print("Choisissez une lettre : ")
-		input, _ := user.ReadString('\n')
-		letter := strings.TrimSpace(strings.ToUpper(input)) // Enlève les espaces au cas ou
-
-		if len(letter) != 1 {
-			fmt.Println("Veuillez saisir une seule lettre.")
-			continue
-		}
-
-		revealedRune := rune(letter[0])
-		if !unicode.IsLetter(revealedRune) {
-			fmt.Println("Ce n'est pas une lettre.")
-			continue
-		}
-
-		if lettersRevealed[revealedRune] {
-			fmt.Println("Vous avez déjà donné cette lettre.")
-			continue
-		}
-		lettersRevealed[revealedRune] = true
-
-		wordFound := false
-		for i, ltr := range word {
-			if unicode.ToUpper(ltr) == revealedRune {
-				revealed[i] = true
-				wordFound = true
-				lastCorrectLetter = ltr
-			}
-		}
-
-		if wordFound {
-			if allRevealed(revealed) {
-				fmt.Println("Bravo ! Le mot est :")
-				displayWord(word, revealed, lastCorrectLetter)
-				return
-			}
-		} else {
-			tentatives-- // Décrémente le nombre de tentatives
-			fmt.Printf("Pas présent dans le mot, il vous reste %d tentatives.\n", tentatives)
-			if maxtentative-tentatives-1 < len(hangmanStages) {
-				fmt.Println(hangmanStages[maxtentative-tentatives-1]) // Affiche l'étape du pendu
-			}
-		}
-
-		if tentatives == 0 {
-			for i := range revealed {
-				revealed[i] = true
-			}
-			fmt.Println("Perdu ! Le mot était :")
-			displayWord(word, revealed, lastCorrectLetter)
-			return
-		}
-
-		displayWord(word, revealed, lastCorrectLetter)
-	}
 }
--- hangmanweb.go
+++ hangmanweb.go
@@ -1,7 +1,119 @@
 package main
 
-import "hangman_web/Hangmanclassic"
+import (
+	"fmt"
+	"hangman_web/Hangmanclassic"
+	"html/template"
+	"log"
+	"net/http"
+	"strings"
+)
+
+var word string
+var revealed []bool
+var attemptsLeft int
+var lettersRevealed map[rune]bool
+
+func initializeGame() {
+	// Initialisation du jeu
+	words, err := Hangmanclassic.ReadFileLines("words.txt", false)
+	if err != nil {
+		fmt.Println("Erreur lors de la lecture du fichier :", err)
+		return
+	}
+	word, revealed = Hangmanclassic.FindWord(words)
+	attemptsLeft = Hangmanclassic.Maxtentative
+	lettersRevealed = make(map[rune]bool)
+}
+
+func renderTemplate(w http.ResponseWriter, data interface{}) {
+	tmpl := `
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<title>Hangman Game</title>
+	</head>
+	<body>
+		<h1>Jeu du Pendu</h1>
+		<p>Mot à deviner : {{.WordDisplay}}</p>
+		<p>Tentatives restantes : {{.AttemptsLeft}}</p>
+		<form action="/hangman" method="POST">
+			<label for="letter">Entrez une lettre :</label>
+			<input type="text" name="letter" maxlength="1" required>
+			<button type="submit">Vérifier</button>
+		</form>
+	</body>
+	</html>
+	`
+	t, err := template.New("hangman").Parse(tmpl)
+	if err != nil {
+		http.Error(w, "Erreur interne du serveur", http.StatusInternalServerError)
+		return
+	}
+
+	t.Execute(w, data)
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	wordDisplay := Hangmanclassic.DisplayWord(word, revealed, ' ')
+	data := struct {
+		WordDisplay  string
+		AttemptsLeft int
+	}{
+		WordDisplay:  wordDisplay,
+		AttemptsLeft: attemptsLeft,
+	}
+	renderTemplate(w, data)
+}
+
+func Hangman(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	letter := r.FormValue("letter")
+	if letter == "" || len(letter) != 1 {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	revealedRune := rune(strings.ToUpper(letter)[0])
+	if lettersRevealed[revealedRune] {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	lettersRevealed[revealedRune] = true
+
+	found := false
+	for i, ltr := range word {
+		if ltr == revealedRune {
+			revealed[i] = true
+			found = true
+		}
+	}
+
+	if !found {
+		attemptsLeft--
+	}
+
+	if attemptsLeft <= 0 || Hangmanclassic.AllRevealed(revealed) {
+		initializeGame() // Réinitialiser le jeu si toutes les lettres sont trouvées ou si les tentatives sont épuisées
+	}
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
 
 func main() {
-	Hangmanclassic.HelloWorld()
+	initializeGame()
+
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/hangman", Hangman)
+
+	port := "8080"
+	fmt.Printf("Serveur démarré sur http://localhost:%s\n", port)
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatalf("Échec du démarrage du serveur : %v", err)
+	}
 }
